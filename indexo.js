60ee2362c7d9bfadd91f6d4de4c62c87ae77591b
Carlo Atripaldi <carloatrica@gmail.com>
2024-01-16 20:54:56 +0100

    Update indexo.js

--- indexo.js
+++ indexo.js
@@ -1,8 +1,32 @@
-const cerca = function () {
-  const artist = document.querySelector("#putsearch");
-  console.log("artist", artist);
-  fetch(
-    "https://striveschool-api.herokuapp.com/api/deezer/search?q=" + artist.value
-  );
+const url = "https://striveschool-api.herokuapp.com/books";
+
+window.onload = () => {
+  fetchBooks();
 };
-cerca();
+const fetchBooks = () => {
+  fetch(url)
+    .then((raw) => raw.json())
+    .then((res) => {
+      let cont = document.querySelector(".img");
+
+      cont.innerHTML = res
+        .map((book) => {
+          return ` <div class='col col-9'> <div class="card mb-4 shadow-sm" id='book_${book.asin}'>
+                <img src='${book.img}' />
+
+                <div class="card-body">
+                  <p class='font-weight-bold text-truncate book-title'> ${book.title} </p>
+                  <div
+                    class="d-flex justify-content-between align-items-center"
+                  >
+                    <button class='btn btn-primary' onclick="addToCart('${book.title}', '${book.price}', '${book.asin}')"> EUR ${book.price} </button>
+                    <button class='btn btn-secondary' onclick='hideCard(event)'> Nascondi </button>
+                    </div>
+                    <a class='btn btn-warning w-100' href='./details.html?id=${book.asin}'> Dettagli </a>
+                </div>
+              </div> </div>`;
+        })
+        .join("");
+    })
+    .catch((err) => console.error(err));
+};
